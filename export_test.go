248c1b1ae61fb83a8481b626d272a7df2bb8c607
Guillaume Sempe <guillaume.sempe@gmail.com>
2022-06-13 15:53:25 +0200

    Add tests and examples

---
+++ export_test.go
@@ -0,0 +1,18 @@
+package aform
+
+var ExportAttributableAttribute = Attributable.attribute
+var ExportRequiredError = requiredError
+
+type ExportTmplAttrs = tmplAttrs
+
+var ExportAttributableListToAttrs = attributableListToAttrs
+
+var ExportValidAutoID = validAutoID
+var ExportNormalizedName = normalizedName
+
+var ExportDefaultAutoID = defaultAutoID
+
+var ExportSelectLanguage = selectLanguage
+var ExportLanguages = languages
+
+var ExportBuildValidationChoicesRule = buildValidationChoicesRule
